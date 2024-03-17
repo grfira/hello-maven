1aebc44663f5db5b862644871c34e4031bb19f3a
fira <gm_c@wp.pl>
2024-03-17 10:47:25 +0100

    first commit

--- src/main/java/pl/edu/agh/mwo/hellomaven/App.java
+++ src/main/java/pl/edu/agh/mwo/hellomaven/App.java
@@ -1,17 +1,34 @@
 package pl.edu.agh.mwo.hellomaven;
 
+import com.indvd00m.ascii.render.Render;
+import com.indvd00m.ascii.render.api.ICanvas;
+import com.indvd00m.ascii.render.api.IContextBuilder;
+import com.indvd00m.ascii.render.api.IRender;
+import com.indvd00m.ascii.render.elements.PseudoText;
+
 /**
- * Simple main application
+ * Simple main application.
  */
 public class App {
 
+    public static final int RANDOM = 4;
+    public static final int WIDTH = 120;
+    public static final int HEIGHT = 20;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        IRender render = new Render();
+        IContextBuilder builder = render.newBuilder();
+        builder.width(WIDTH).height(HEIGHT);
+        builder.element(new PseudoText("Hello Maven"));
+        ICanvas canvas = render.render(builder.build());
+        String s = canvas.getText();
+        System.out.println(s);
     }
-    
+
     public int getRandomNumber() {
         // chosen by fair dice roll.
         // guaranteed to be random
-        return 4;
+        return RANDOM;
     }
 }
